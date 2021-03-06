8f60f973ab9c54469380d4139f3b405282ed5407
Nathan Lee <nathan.lee@gyana.co.uk>
2021-03-06 02:03:32 +0000

    add test cases for Text component

---
+++ src/components/Text/Text.test.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { SubText } from './Text';
+import React from 'react';
+import { expect, should } from 'chai';
+
+describe('SubText', () => {
+  it('should display the text', () => {
+    const TEXT = 'hello';
+    const { queryByText } = render(<SubText>{TEXT}</SubText>);
+    const text = queryByText(TEXT);
+    should().exist(text);
+  });
+
+  it('should be empty if no text is given', () => {
+    const { queryByText } = render(<SubText></SubText>);
+    should().not.exist(queryByText('.*'));
+  });
+});
